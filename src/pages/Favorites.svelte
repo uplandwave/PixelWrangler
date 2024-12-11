ceca4aebc4c0b718a4c54af8aecfdae30b3f7912
starb520 <97459015+starb520@users.noreply.github.com>
2024-12-10 17:49:09 -0700

    deleted an extra import

--- src/pages/Favorites.svelte
+++ src/pages/Favorites.svelte
@@ -3,7 +3,6 @@
   import { user, route } from "../stores.js"; 
   import { fetchFavoritesData } from "../utils/supabaseFetchers.mjs";
   import MovieCard from "../lib/MovieCard.svelte";
-  import MovieModal from "./MovieModal.svelte";
 
   let favorites = [];
   let message = "";
